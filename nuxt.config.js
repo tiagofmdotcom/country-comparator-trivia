a42bbf4050585fc97cc3e28e2f10065a6be6c04e
I-NOZex <tiagofm.profissional@outlook.pt>
2022-01-28 19:10:35 +0100

    move config files to root

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,22 @@
+/* eslint-disable */
 export default {
-	srcDir: 'front/',
+  srcDir: "front/",
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'country-compare',
+    title: "country-compare",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -30,49 +31,61 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    "@nuxtjs/stylelint-module",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en',
+      lang: "en",
     },
   },
 
   generate: {
-    dir: '../docs'
+    dir: "../docs",
   },
 
   router: {
-    base: `/${process.env.ROUTER_BASE}` ?? '/',
-  }
+    base: `/${process.env.ROUTER_BASE}` ?? "/",
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  /* build: {
-    loaders: {
+  build: {
+    extend: (config) => {
+      const svgRule = config.module.rules.find((rule) =>
+        rule.test.test(".svg")
+      );
+
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/;
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["babel-loader", "vue-svg-loader"],
+      });
+    },
+    /*loaders: {
       sass: {
         implementation: require('sass'),
       },
       scss: {
         implementation: require('sass'),
       },
-    },
-  } */
-}
+    },*/
+  },
+};
