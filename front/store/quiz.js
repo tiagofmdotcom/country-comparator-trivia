cdb2b0655c53308d024379249de163d8771fe5d4
I-NOZex <tiagofm.profissional@outlook.pt>
2022-03-22 11:56:03 +0100

    ux elective delivery

--- front/store/quiz.js
+++ front/store/quiz.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
 
+/**
+ * Extracts integer from given Object and Key, returning 0 if NaN
+ * @param {Object} obj
+ * @param {any} key
+ * @param {any} prop=null
+ * @returns {Number}
+ */
+const getInt = (obj, key, prop = null) => {
+    if(prop === null)
+        return obj[key] ?? 0;
+    else
+        return obj[key]?.[prop] ?? 0;
+}
+
 export const state = () => ({
     countries: [],
     questions: [],
@@ -23,6 +37,58 @@ export const getters = {
     choosenAnswers: (state) => {
         return state.choosenAnswers
     },
+
+    correctAnswers: (state) => {
+        return state.choosenAnswers.filter(a => a.answer.isCorrect)
+    },
+
+    totalTime: (state) => {
+        return state.choosenAnswers.reduce((total, curr) => {
+            return total + curr.elapsedAnswerTime
+        }, 0);
+    },
+
+    countryNames: (state) => {
+        return state.countries.map(country => country.name)
+    },
+
+    priceCategories: (state) => {
+        return  [...new Set( // a set is a fast way to make a distinct array
+            state.countries[0]?.prices?.reduce((total, curr) => {
+                const items = curr?.item_name?.split(',');
+                        // item name (eg: Restaurants)
+                return [...total, ...[items[items.length-1].trim()]]
+            }, [])
+        )];
+    },
+    
+    countriesFilterPricesPerCategory: (state) => {
+        return state.countries.reduce((countries, country) => {
+            return {
+                ...countries,
+                ...{[country.name]: country.prices
+                    .map(p => {
+                        const items = p?.item_name?.split(',');
+                                // item name (eg: Restaurants)
+                        return {[items[items.length-1].trim()]: p?.average_price ?? 0}
+                    })
+                    .reduce((total, curr) => {
+                        return {
+                            ...total, 
+                            ...{
+                                // item name (eg: Restaurants) : calculate average
+                                [Object.keys(curr)[0]]: {
+                                    total: getInt(total, Object.keys(curr)[0], 'total') + getInt(curr, Object.keys(curr)[0]),
+                                    count: getInt(total, Object.keys(curr)[0], 'count') + 1,
+                                    get avg() { return this.total / this.count }
+                                }
+                            }
+                        }
+                    }, {})
+                }
+            }
+        }, {})
+    }
 }
 
 /* export const actions = {
