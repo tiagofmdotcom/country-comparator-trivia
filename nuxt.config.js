367870e3bd86693df69875d24aa84f1bc2cd03af
Tiago Marques <tiago.marques@zendesk.com>
2022-05-10 16:38:15 +0200

    [front] #78 fetch data from numbeo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,85 +1,88 @@
 /* eslint-disable */
 export default {
-  srcDir: "front/",
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+    srcDir: 'front/',
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "country-compare",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'country-compare',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+        // https://go.nuxtjs.dev/stylelint
+        '@nuxtjs/stylelint-module',
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+        // https://go.nuxtjs.dev/pwa
+        '@nuxtjs/pwa'
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    "@nuxtjs/eslint-module",
-    // https://go.nuxtjs.dev/stylelint
-    "@nuxtjs/stylelint-module",
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
-    // https://go.nuxtjs.dev/pwa
-    "@nuxtjs/pwa",
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "/",
-  },
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: "en",
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: '/'
     },
-  },
 
-  generate: {
-    dir: "docs/",
-  },
-
-  router: {
-    base: `/${process.env.ROUTER_BASE || ""}`,
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    extend: (config) => {
-      const svgRule = config.module.rules.find((rule) =>
-        rule.test.test(".svg")
-      );
-
-      svgRule.test = /\.(png|jpe?g|gif|webp)$/;
-
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["babel-loader", "vue-svg-loader"],
-      });
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+        manifest: {
+            lang: 'en'
+        }
     },
-    /*loaders: {
+
+    generate: {
+        dir: 'docs/'
+    },
+
+    router: {
+        base: `/${process.env.ROUTER_BASE || ''}`
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        extend: (config) => {
+            const svgRule = config.module.rules.find((rule) =>
+                rule.test.test('.svg')
+            );
+
+            svgRule.test = /\.(png|jpe?g|gif|webp)$/;
+
+            config.module.rules.push({
+                test: /\.svg$/,
+                use: ['babel-loader', 'vue-svg-loader']
+            });
+        }
+        /*loaders: {
       sass: {
         implementation: require('sass'),
       },
@@ -87,5 +90,9 @@ export default {
         implementation: require('sass'),
       },
     },*/
-  },
+    },
+    publicRuntimeConfig: {
+        NUMBEO_API_KEY: process.env.NUMBEO_API_KEY,
+        OFFLINE_MODE: process.env.OFFLINE_MODE,
+    }    
 };
